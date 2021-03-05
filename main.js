2545380699891938d09ee315d245eb9c7c3377b8
Ivan Ruiz <ivanrur95@hotmail.com>
2021-03-05 17:22:57 -0600

    se agrega ventana modal, y se ajustan aptitudes, falta la version de pc quedan bloquen bloques de dos, y no de tres

--- main.js
+++ main.js
@@ -2,9 +2,11 @@ let burgerMenu = document.getElementById('burger_menu');
 let menuMovil = document.getElementById('menu_movil');
 let contador = 0;
 let icono = document.querySelector('.fas.fa-bars')
+let ubicacionPrincipal  = window.pageYOffset;
 
 burgerMenu.addEventListener('click',function(){
     if(contador == 0 ){
+        
         menuMovil.className = menuMovil.className + ' active'
         icono.classList.replace = ('fas fa-bars')
         icono.className = ('fas fa-times')
@@ -17,6 +19,25 @@ burgerMenu.addEventListener('click',function(){
         icono.className = ('fas fa-bars')
         icono.style.transform = "rotate(180deg)";
         contador = 0
-        
+
     }
 })
+
+
+window.onscroll = function() {
+    let Desplazamiento_Actual = window.pageYOffset;
+    if(ubicacionPrincipal >= Desplazamiento_Actual){
+        
+        document.getElementById('main-menu').style.top = '0';
+        document.getElementById('main-menu').style.transition = '.2s ease-in';
+
+        
+    }
+    else{
+        
+        document.getElementById('main-menu').style.top = '-120px';
+        document.getElementById('main-menu').style.transition = '.2s ease-in';
+
+    }
+    ubicacionPrincipal = Desplazamiento_Actual;
+}
