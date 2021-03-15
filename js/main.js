7dec58a4b86b6a175e3ed8570ee04723b03d557d
Ivan Ruiz <ivanrur95@hotmail.com>
2021-03-15 00:25:37 -0600

    se eliminan los archivos de prueba y se quita la liereria de Jqeury de pruebas falta cambios a seccion de aptitudes y formulario de contacto, agregar nueva pagina de trabajos como opción en el menú

--- js/main.js
+++ js/main.js
@@ -41,3 +41,4 @@ window.onscroll = function() {
     }
     ubicacionPrincipal = Desplazamiento_Actual;
 }
+
---
+++ js/window.js
@@ -0,0 +1,7 @@
+let modalAlert = document.getElementById('modal_alerta')
+let boton =  document.getElementById('cerrar_modal')
+
+boton.addEventListener('click', function(){
+    modalAlert.classList = ('d-none')
+    modalAlert.style.transition = "all 2s ease-in";
+})
